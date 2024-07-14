5a1e19c2dc0bca2ed7c43336dda60a3f4c9c26a9
nguyenanh1997dz <nguyenanh1997dz@gmail.com>
2024-07-14 18:41:47 +0700

    a

--- model/foodModel.js
+++ model/foodModel.js
@@ -1,34 +1,38 @@
 const mongoose = require("mongoose");
 
 const foodSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,  
-        required: true,
-    },
-    image: {
-        type: Object
-    },
-    status: {
-        type: String,
-        enum: ['còn hàng', 'hết hàng'],  
-        default: "còn hàng",
-    },
-    totalSales: {
-        type: Number,
-        default: 0,
-    },
-    quantity: {
-        type: Number,
-        default: 100,
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: Object,
+  },
+  status: {
+    type: String,
+    enum: ["còn hàng", "hết hàng"],
+    default: "còn hàng",
+  },
+  totalSales: {
+    type: Number,
+    default: 0,
+  },
+  totalSalesPrice: {
+    type: Number,
+    default: 0,
+  },
+  quantity: {
+    type: Number,
+    default: 100,
+  },
 });
 
 module.exports = mongoose.model("Food", foodSchema);
