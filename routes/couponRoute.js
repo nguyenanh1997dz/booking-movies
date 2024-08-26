48300378d824ec0c22c682c87ec6ac0303ae116d
chnguyenanh47 <ch.nguyenanh47@gmail.com>
2024-08-26 23:01:35 +0700

    xóa mã giảm giá

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -3,6 +3,7 @@ const CouponController = require("../controller/couponCtrl");
 const router = express.Router();
 const { authMiddleware, isAdmin} = require("../middleware/authMiddlewere");
 router.get('/', CouponController.getAll)
+router.delete('/:id', CouponController.delete)
 router.post('/',authMiddleware,isAdmin, CouponController.create)
 router.post("/check",CouponController.checkCode)
 module.exports = router;
